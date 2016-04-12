90581be969a33710ac32c28aa43b584fc090ca25
saeoshi <eswat.dunan@gmail.com>
2016-04-12 10:28:13 +0900

    fixed url

--- referer.py
+++ referer.py
@@ -9,8 +9,7 @@ import sys
 opener=urllib2.build_opener()
 ref = open('/Users/saeoshi/ref.txt')
 line = ref.readlines()
-#url = "http://stat.ameba.jp/user_images/20130905/11/ab-anna06/74/d6/j/o0300040012673355938.jpg"
-url = "http://stat.profile.ameba.jp/profile_images/20110928/01/fe/e9/j/o064004801317141126182.jpg"
+url = "http://hogehoge.com/01/fe/e9/j/o064004801317141126182.jpg"
 
 for x in line :
         try:
